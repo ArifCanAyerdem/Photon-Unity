e9837f4bdff93c44f10807323e12786c0e2eddec
Arif Can AYERDEM <arifcan_2000@hotmail.com>
2024-10-25 21:39:05 +0300

    +create script

---
+++ Assets/Scripts/FirstLessonScript.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace lessonBasic
+{ 
+
+public class FirstLessonScript : MonoBehaviour
+{
+   
+
+        public int tamSayi=1;
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+}
